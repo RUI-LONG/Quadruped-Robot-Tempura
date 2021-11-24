bd8c85c7e154d12294085bbfa9640b567ecf4fb2
RUI-LONG <m61216051116@gmail.com>
2021-11-24 12:00:47 +0800

    Update hand control dependency

--- hand_control.py
+++ hand_control.py
@@ -20,12 +20,18 @@ an initial event.
 """
 import time
 from Movement.movement import Skills
+from Movement.pid import PID
 from Raspi_I2C.matrix_servo_extension import MatrixServoExtension
 from depthai_hand_tracker.HandController import HandController
 
+from numpy import interp
+
 device = MatrixServoExtension()
 skill = Skills(device)
-print("Device Connected:", device.begin())
+
+status = device.begin()
+print("Device Connected:", status)
+pid = PID(1, 0.01, 0.01)
 time.sleep(2)
 
 def StandOrSit(event):
@@ -40,14 +46,13 @@ def pushUpOrNot(event):
     if hand_ang > 1 or hand_ang < -1:
         skill.pushUp()
     else:
-        skill.standUp()
+        skill.standBy(70)
 
 def shakeRightHand(event):
-    event.print_line() 
+    # event.print_line() 
     skill.shakeRightHand()
 
 def shakeLeftHand(event):
-    event.print_line() 
     skill.shakeLeftHand()
 
 def stretch(event):
@@ -62,29 +67,42 @@ def carzy(event):
 def yoga(event):
     skill.yoga()
 
+def walk(event):
+    skill.walk(1)
+
 def trackBall(event):
     pass
 
-def walkForward(event):
+def warmUp(event):
+    skill.warmUp()
+    
+def traceFist(event):
+    # TODO
+    # cam_width = 1152
+    # cam_height = 648
+    # error = int((576 - int(event.hand.rect_x_center_a))/20)
+    # output = pid.update(error, debug=True)
+    # sevro_adjust = int(interp(output,[-400, 400],[-90, 90]))
+    # print(sevro_adjust)
+    # print("X: ", int(event.hand.rect_x_center_a), "Y: ",int(event.hand.rect_y_center_a))
     pass
 
-def walkBackward(event):
-    pass
+def stop(event):
+    skill.standBy(50)
 
 
-# ALL_POSES = ["ONE","TWO","THREE","FOUR","FIVE", "ROCK", "FIST","SEVEN","OK","FUCK1","FUCK2"]
-# Registed = ["THREE", "FOUR", "OK"]
-
+# ALL_POSES = ["ONE","TWO","THREE","FOUR","FIVE", "ROCK", "FIST","SEVEN","NICE","FUCK1","FUCK2"]
+# Registed = ["THREE", "FOUR", "NICE"]
 
 config = {
     'renderer' : {'enable': False},
     
     'pose_actions' : [
         {'name': 'StandOrSit', 'pose':['ONE', 'SEVEN'], 'hand':'left', 'callback': 'StandOrSit', "trigger":"enter", 
-            "first_trigger_delay":0.1, "next_trigger_delay": 0},
+            "first_trigger_delay":0.15, "next_trigger_delay": 0},
 
         {'name': 'pushUpOrNot', 'pose':['ONE', 'SEVEN'], 'hand':'right', 'callback': 'pushUpOrNot', "trigger":"enter", 
-            "first_trigger_delay":0.1, "next_trigger_delay": 0},
+            "first_trigger_delay":0.15, "next_trigger_delay": 0},
 
         {'name': 'stretch', 'pose':'TWO', 'callback': 'stretch', "trigger":"enter", 
             "first_trigger_delay":0.2, "next_trigger_delay": 0},
@@ -98,15 +116,30 @@ config = {
         {'name': 'shakeLeftHand', 'pose': 'FIVE', 'hand':'left', 'callback': 'shakeLeftHand', "trigger":"enter", 
             "first_trigger_delay":0.2, "next_trigger_delay": 0},
         {'name': 'shakeRightHand', 'pose': 'FIVE', 'hand':'right', 'callback': 'shakeRightHand', "trigger":"enter", 
-            "first_trigger_delay":0.2, "next_trigger_delay": 0}
+            "first_trigger_delay":0.2, "next_trigger_delay": 0},
+
+        {'name': 'walk', 'pose': 'FOUR', 'callback': 'walk', "trigger":"periodic", 
+            "first_trigger_delay":0.2, "next_trigger_delay": 0},
+
+        {'name': 'stop', 'pose': 'FIST', 'callback': 'stop', "trigger":"enter",
+            "first_trigger_delay":0.2, "next_trigger_delay": 0},
+
+        {'name': 'warmUp', 'pose': 'NICE', 'callback': 'warmUp', "trigger":"enter",
+            "first_trigger_delay":0.2, "next_trigger_delay": 0},
+
+        # {'name': 'traceFist', 'pose': 'FIST', 'hand':'right', 'callback': 'traceFist', "trigger":"continuous",
+        #     "first_trigger_delay":0, "next_trigger_delay": 0},
+
     ]
 }
 
 try:
-    skill.standBy()
-    HandController(config).loop()
-except KeyboardInterrupt:
+    if status:
+        skill.standBy()
+        HandController(config).loop()
+except:
+    device.close()
     pass
-device.close()
+
 time.sleep(2)
 print("Device Closed")
--- test_servo.py
+++ test_servo.py
@@ -4,27 +4,46 @@ from Movement.movement import Skills
 
 device = MatrixServoExtension()
 skill = Skills(device)
-print("Device Connected:", device.begin())
-time.sleep(2)
-_body = {"LF": skill.LeftF, "lf": skill.LeftF, "LB": skill.LeftB, "lb": skill.LeftB,
-    "RF": skill.RightF, "rf": skill.RightF, "RB": skill.RightB, "rb": skill.RightB,
-    "su": skill.standUp, "ld":skill.layDown, "ld2":skill.layDown2 }
 
-while True:
-    try:
-        _input = input("Input Angle:").split(" ")
-        if len(_input) == 1:
-            _body[_input[0]]()
-        else:
-            if isinstance(_input[2], int):
+device_status = device.begin()
+if device_status:
+    time.sleep(2)
+    _body = {
+        "lf": skill.LeftF, "lb": skill.LeftB, "rf": skill.RightF,  "rb": skill.RightB, "hd": skill.headTurn,
+
+        "sb": skill.standBy, "su": skill.standUp, "pu": skill.pushUp, "ld":skill.layDown,
+        "sd": skill.sitDown,  "shr": skill.shakeRightHand, "shl": skill.shakeLeftHand,
+        "stf": skill.stretchF, "ca": skill.crazy, "wa": skill.walk, "yo": skill.yoga, "wu":skill.warmUp,
+        # TODO
+        "gd":skill.getDown, 
+        "t1": skill.t1, "t2": skill.t2, "t3": skill.t3, "t4": skill.t4,
+    }
+
+    skill.standBy(50)
+
+    while True:
+        try:
+            _input = input("Input Angle:").split(" ")
+            if len(_input) == 1:
+                _body[_input[0]]()
+            elif len(_input) == 3:
                 _body[_input[0]](int(_input[1]), int(_input[2]))
             else:
-                _body[_input[0]](int(_input[1]), -1)
-            
-    except:
-        break
+                _body[_input[0]](int(_input[1]))
 
-# time.sleep(2)
+            print("rf:", skill.rf_a, skill.rf_h, ",",
+                    "rb:", skill.rb_a, skill.rb_h)
+            print("lf:", skill.lf_a, skill.lf_h, ",",
+                    "lb:", skill.lb_a, skill.lb_h)
+        except KeyboardInterrupt:
+            break
+        except KeyError:
+            print("KeyError")
+            pass
+        except Exception as e:
+            print(e)
+            break
+
 device.close()
-time.sleep(1)
+time.sleep(2)
 print("Device Closed")
