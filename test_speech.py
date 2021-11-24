c6c91bc7a24a5866e6878d39096fc17812930a63
RUI-LONG <m61216051116@gmail.com>
2021-11-24 12:02:13 +0800

    Remove Unused Files

--- test_speech.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-raw_audio = sr.AudioFile('raw_audio.wav')
-with raw_audio as source:
-    audio = r.record(source)
-
-text = r.recognize_google(audio, show_all=False, language="zh-CN")
-print(text)
